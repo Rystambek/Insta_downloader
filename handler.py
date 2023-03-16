6ec2d8b00147f0bd244e25a19f0a6fc9d791c2fc
Rystambek <rustambekjumaqulov@gmail.com>
2023-03-16 22:16:43 +0500

    baza

--- handler.py
+++ handler.py
@@ -1,13 +1,18 @@
 from telegram import Update,ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardButton,InlineKeyboardMarkup
 from telegram.ext import Updater, Filters, CallbackContext, MessageHandler, CallbackQueryHandler, CommandHandler
 import requests
+from db import DB
+db = DB('db.json')
 
 def start(update:Update,context:CallbackContext):
     bot = context.bot
     chat_id = update.message.chat.id
+    user_name = update.message.from_user.first_name
     text = "⛔️ *Botdan to'liq foydalanish uchun* quyidagi kanallarga obuna bo'ling"
-    chanel_1 = "https://t.me/Rustambek03_IT_blog"
-    chanel_2 = "https://t.me/Rustambek15649"
+    db.starting(chat_id=chat_id,user_name=user_name)
+    db.save()
+    chanel_1 = db.get_channel()[0]
+    chanel_2 = db.get_channel()[1]
     keyboard = InlineKeyboardMarkup([
         [InlineKeyboardButton('➕ kanalga qo\'shilish',url = chanel_1),InlineKeyboardButton('➕ kanalga qo\'shilish',url = chanel_2)],
         [InlineKeyboardButton('Tekshirish',callback_data='tekshirish')],
@@ -22,8 +27,8 @@ def tekshir(update:Update, context:CallbackContext):
     user_name = query.message.chat.first_name
     message_id = query.message.message_id
     user_id = query.from_user.id
-    chanel_1 = 'https://t.me/Rustambek03_IT_blog'
-    chanel_2 = 'https://t.me/Rustambek15649'
+    chanel_1 = db.get_channel()[0]
+    chanel_2 = db.get_channel()[1]
     chanel1 = bot.getChatMember(f"@{chanel_1[13:]}",chat_id)['status']
     chanel2 = bot.getChatMember(f"@{chanel_2[13:]}",chat_id)['status']
     print(chanel1)
@@ -38,7 +43,7 @@ def tekshir(update:Update, context:CallbackContext):
 
 🚀 Media yuklashni boshlash uchun uning havolasini yuboring.
 😎 Bot guruhlarda ham ishlay oladi!"""
-        bot.edit_message_text(chat_id=user_id,text=text,message_id=message_id,parse_mode="MarkdownV2")
+        bot.edit_message_text(chat_id=user_id,text=text,message_id=message_id)
 
     else:
         text = "Kanallarga a'zo bo'lmadingiz"
