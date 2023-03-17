3a440f3c37613ec484db53885055237c0947d35e
Rystambek <rustambekjumaqulov@gmail.com>
2023-03-17 15:30:27 +0500

    download video and image

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from telegram import Update,ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardButton,InlineKeyboardMarkup
 from telegram.ext import Updater, Filters, CallbackContext, MessageHandler, CallbackQueryHandler, CommandHandler
-from handler import start,tekshir
+from handler import start,tekshir,download
 import os
 
 TOKEN = "6004154698:AAEo2pZT8WqoCqRGAXZchoYMtdgozcq3Kbc"
@@ -10,5 +10,6 @@ updater = Updater(token=TOKEN)
 dp = updater.dispatcher
 dp.add_handler(CommandHandler('start',start))
 dp.add_handler(CallbackQueryHandler(tekshir,pattern='tekshirish'))
+dp.add_handler(MessageHandler(Filters.update,download))
 updater.start_polling()
 updater.idle()
